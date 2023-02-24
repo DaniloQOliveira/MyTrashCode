596a9f90ca5dc1ef381a5a1d893eec882a093f9a
DaniloQOliveira <dqoliveira21@gmail.com>
2023-02-24 15:45:37 +0000

    added Exp.hs

---
+++ MEI/2022-2023/2sem/MES/Exp.hs
@@ -0,0 +1,40 @@
+-- Perfil em Eng. de Sw
+-- Aula de 24/2/2023
+
+module Parser where
+
+import Parser
+import Let
+import Prelude hiding ((<*>),(<$>))
+import Data.Char
+
+data Exp = Add Exp Exp
+         | Mul Exp Exp
+         | Const Int
+         | Var String
+         deriving String
+
+-- "3 + 4 * 2"
+e :: Exp
+e = Add (Const 3) (Mul (Const 4) (Const 2))
+
+-- Gramática
+-- Exp    -> Termo '+' Exp
+--         | Termo
+-- Termo  -> Factor '*' Termo
+--         | Factor
+-- Factor -> int
+--         | var
+--         | '(' Exp ')'
+
+pExp :: Parser Exp
+
+
+
+
+
+
+
+
+
+
